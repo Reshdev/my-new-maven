25d677fb2e292dd8d12d371c71a6f837c42e23a0
Administrator <Administrator@5CG9378HNN.mshome.net>
2023-06-17 19:25:51 +0530

    commit workbook

---
+++ mvn/src/main/java/workbook/WorkSample2.java
@@ -0,0 +1,14 @@
+package workbook;
+
+import java.io.IOException;
+
+public class WorkSample2 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(WorkSample1.readStringData(1, 1));
+		System.out.println(WorkSample1.readIntegerData(1, 0));
+		// TODO Auto-generated method stub
+
+	}
+
+}
